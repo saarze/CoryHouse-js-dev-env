56be177aaec1722540687cd5524e139ec2687036
Yash Handa <yashhanda7@yahoo.com>
2018-10-20 02:50:25 +0530

    Module 5: Development Web Server

---
+++ buidScripts/srcServer.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const open = require('open');
+
+const port = 1998;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+const server = app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+    console.log(`Server is running at ${port} ...`);
+  }
+});
